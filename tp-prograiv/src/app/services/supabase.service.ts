c972d32afc3fce39cf484ae22281c320ab3bbfee
JuancrCarvallo <juancruz@tots.agency>
2026-09-16 11:45:48 -0300

    supabase config

---
+++ tp-prograiv/src/app/services/supabase.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+  private readonly client: SupabaseClient = createClient(
+    environment.supabaseUrl,
+    environment.supabaseKey,
+  );
+
+  get supabase(): SupabaseClient {
+    return this.client;
+  }
+}
---
+++ tp-prograiv/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  supabaseUrl: 'https://xutkvrixxgkcyoatpvob.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1dGt2cml4eGdrY3lvYXRwdm9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1MDg1NTMsImV4cCI6MjEwNTA4NDU1M30.39umjt9oZsBKcvpKtqx7DbdL_OZS3ytFXDh0UNB6KAs',
+};
